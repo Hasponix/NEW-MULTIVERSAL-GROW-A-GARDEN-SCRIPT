9738b5f1195ceabe36e9881c1a5960cdc1d88d01
Hasponix <arcadymic@gmail.com>
2025-07-19 13:09:47 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/NtINNk5B/raw"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/rzffqMWIjq/0",true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
