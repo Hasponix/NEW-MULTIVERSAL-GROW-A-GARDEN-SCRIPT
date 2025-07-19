6aaf3692d0a51f50e07b49450997f348ff31dc37
Hasponix <arcadymic@gmail.com>
2025-07-19 13:11:59 +0800

    Update true.lua

--- true.lua
+++ true.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/aUbfZ1Wn/raw"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/rzffqMWIjq/0",true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hasponix/NEW-MULTIVERSAL-GROW-A-GARDEN-SCRIPT/refs/heads/main/text.txt"))()
