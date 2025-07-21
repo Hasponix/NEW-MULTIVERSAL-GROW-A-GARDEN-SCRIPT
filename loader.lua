9b17b1bbfee826ac627d0d3a9d2d2e09818e80c6
Hasponix <arcadymic@gmail.com>
2025-07-21 14:46:50 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/rzffqMWIjq/0",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hasponix/NEW-MULTIVERSAL-GROW-A-GARDEN-SCRIPT/refs/heads/main/script.lua"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
